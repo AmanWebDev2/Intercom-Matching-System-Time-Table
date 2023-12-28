fec04c73ce13494c2dba795246caa4f61648b92e
AmanWebdev2 <webdevaman2@gmail.com>
2023-12-28 11:09:20 +0530

    added some methods and ui

--- src/components/time-table/TimeTableGrid.jsx
+++ src/components/time-table/TimeTableGrid.jsx
@@ -1,15 +1,56 @@
 import { useEffect, useState } from 'react'
-import { createData } from './timeTableData'
+import { createData, mergeConsecutiveSchedules } from './timeTableData'
 import "./timeTable.css";
 
 const TimeTableGrid = () => {
   const [schedule,setSchedule] = useState([]);
+  const [matchingSchedule,setMatchingSchedule] = useState([]);
+
   useEffect(()=>{
     const data = createData();
     setSchedule(data);
-    console.log(data)
   },[]);
 
+  useEffect(()=>{
+    console.log(matchingSchedule)
+  },[matchingSchedule]);
+
+  const handleSelectSchedule=(e,selecedSchedule)=>{
+    if(matchingSchedule.length==0){
+      setMatchingSchedule([selecedSchedule]);
+      return;
+    }
+    const isAlreadyExist = matchingSchedule.find((schedule) => schedule.start_minute == selecedSchedule.start_minute);
+
+    if(isAlreadyExist) {
+      let filteredSchedule = matchingSchedule.filter((schedule) => (schedule.start_minute !== selecedSchedule.start_minute) && (schedule.end_minute !== selecedSchedule.end_minute));
+      setMatchingSchedule([...filteredSchedule]);
+    }else {
+      // check if selection is consecutive
+      const sch = [...matchingSchedule,selecedSchedule];
+      const data = mergeConsecutiveSchedules(sch);
+      setMatchingSchedule(data);
+    }
+
+  }
+
+  const handleIsActive=(data)=>{
+    if(matchingSchedule.length==0){
+      return false;
+    }
+    const isFound = matchingSchedule.find((sch)=>sch?.start_minute === data?.start_minute);
+    if(isFound) {
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+
+  
+  const gridItemSelectionLogic=(schedule,day)=> {
+   return `${day} ${schedule.readableStartTime}-${schedule.readableEndTime}`
+  }
 
   return (
     <main className='grid-time-matching'>
@@ -36,10 +77,12 @@ const TimeTableGrid = () => {
               {
                 data.time.map((min,i)=>{
                   return(
-                    <div key={i} className={`timetable-editor-grid-item ${i==6 && 'active'}`}>
-                      <span className="timetable-editor-grid-item-hover-text">
+                    <div key={i} className={`timetable-editor-grid-item ${handleIsActive(min) && 'active'}`} onClick={(e)=>{handleSelectSchedule(e,{day:data.day,...min})}}>
+                      {handleIsActive(min) ? 
+                        gridItemSelectionLogic(min,data.day)
+                      :<span className="timetable-editor-grid-item-hover-text">
                         {`${data.day} ${min.readableStartTime}`} 
-                      </span>
+                      </span>}
                     </div>
                   )
                 })
--- src/components/time-table/timeTableData.js
+++ src/components/time-table/timeTableData.js
@@ -1,6 +1,6 @@
 const day = ["Mon", "Tues", "Weds", "Thurs", "Fri", "Sat", "Sun"];
 
-export function createData() {
+function createData() {
     const finalData = [];
     const minInADay = 24 * 60;
   
@@ -34,6 +34,72 @@ export function createData() {
   
     return finalData;
   }
+
+  function mergeConsecutiveSchedules(schedules) {
+    if (schedules.length <= 1) {
+      return schedules; // No need to merge if there's only one or no schedule
+    }
+  
+    // Sort schedules based on start_minute
+    const sortedSchedules = schedules.slice().sort((a, b) => a.start_minute - b.start_minute);
+  
+    const mergedSchedules = [];
+    let currentSchedule = { ...sortedSchedules[0] };
+  
+    for (let i = 1; i < sortedSchedules.length; i++) {
+      const nextSchedule = sortedSchedules[i];
+  
+      if (
+        currentSchedule.day === nextSchedule.day &&
+        currentSchedule.end_minute >= nextSchedule.start_minute
+      ) {
+        // Merge overlapping or consecutive schedules
+        currentSchedule.end_minute = Math.max(currentSchedule.end_minute, nextSchedule.end_minute);
+        currentSchedule.readableEndTime = nextSchedule.readableEndTime;
+      } else {
+        // Non-consecutive or different day, push the current schedule and update currentSchedule
+        mergedSchedules.push(currentSchedule);
+        currentSchedule = { ...nextSchedule };
+      }
+    }
+  
+    // Push the last schedule after the loop
+    mergedSchedules.push(currentSchedule);
+  
+    return mergedSchedules;
+  }
+  
+  function splitSchedules(schedules) {
+    const splitSchedules = [];
+  
+    for (const schedule of schedules) {
+      const duration = schedule.end_minute - schedule.start_minute;
+  
+      if (duration === 60) {
+        splitSchedules.push(schedule);
+      } else {
+        // Split the schedule into multiple 60-minute schedules
+        let currentStartMinute = schedule.start_minute;
+        let currentEndMinute = currentStartMinute + 60;
+  
+        while (currentEndMinute <= schedule.end_minute) {
+          splitSchedules.push({
+            day: schedule.day,
+            start_minute: currentStartMinute,
+            end_minute: currentEndMinute,
+            readableStartTime: formatReadableTime(currentStartMinute),
+            readableEndTime: formatReadableTime(currentEndMinute),
+          });
+  
+          currentStartMinute = currentEndMinute;
+          currentEndMinute += 60;
+        }
+      }
+    }
+  
+    return splitSchedules;
+  }
+  
   
   function formatReadableTime(minutes) {
     const adjustedMinutes = minutes % (24 * 60); // Ensure minutes stay within a single day
@@ -41,3 +107,5 @@ export function createData() {
     const period = adjustedMinutes < 720 ? 'AM' : 'PM';
     return `${hour}:${(adjustedMinutes % 60).toString().padStart(2, '0')}${period}`;
   }
+
+  export { formatReadableTime,mergeConsecutiveSchedules,createData,splitSchedules }
